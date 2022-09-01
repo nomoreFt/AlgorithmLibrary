bf61d596fced4c6b5b9f0bb90ee42accf0938a3d
nomoreFt <hyunwoo.kim1108@gmail.com>
2022-09-01 23:56:22 +0900

    fix

--- Main.java
+++ Main.java
@@ -1,28 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
 
 public class Main {
-    static int N;
-    static int nX[] = {1,-1,0,0};
-    static int nY[] = {0, 0, 1, -1};
-    static int[][] graph;
+    static int n;
+    static int dp[];
+    static int dpR[];
+    static int arr[];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        graph = new int[N][N];
-
-        int danzi = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if(graph[i][j] == 0) continue;
-                danzi++;
-            }
+        n = Integer.parseInt(br.readLine());
+        dp = new int[n+1];
+        arr = new int[n + 1];
+        dpR = new int[n + 1];
+        String[] strs = br.readLine().split(" ");
+        for (int i = 0; i < strs.length; i++) {
+            arr[i + 1] = Integer.parseInt(strs[i]);
         }
-    }
 
-    static void dfs(int x, int y) {
+        dp[1] = arr[1];
+        int result = dp[1];
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.max(arr[i], dp[i - 1] + arr[i]);
+            result = Math.max(result, dp[i]);
+        }
 
+        for (int i = 1; i <= n; i++) {
+
+        }
+        System.out.println(result);
     }
 }
